4f7c51cc2ed3dbe386d46028c26831c79f3173d2
VikasWatore <watorevikas@gmail.com>
2025-02-13 21:17:06 +0530

    moblie View Changes

--- src/components/navbar/NavbarMain.jsx
+++ src/components/navbar/NavbarMain.jsx
@@ -1,31 +1,26 @@
-import React, { useState } from 'react'
-import NavbarLogo from './NavbarLogo'
-import NavbarLinks from './NavbarLinks'
-import NavbarBtn from './NavbarBtn'
-import { GiHamburgerMenu } from "react-icons/gi";
+import NavbarLogo from "./NavbarLogo";
+import NavbarLinks from "./NavbarLinks";
+import NavbarBtn from "./NavbarBtn";
+import NavbarToggler from "./NavbarToggler";
+import { useSelector } from "react-redux";
 
-function NavbarMain() {
-  const [menuOpen,setMenuOpen]=useState(false);
-  const toggleMenu=()=>{
-    setMenuOpen(!menuOpen)
-  }
+const NavbarMain = () => {
+  const menuOpen = useSelector((state) => state.menu.menuOpen);
   return (
-    <nav className='max-w-[1300px] mx-auto w-full px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-4'>
-     <div className='flex justify-between w-full max-w-[1200px] mx-auto bg-black items-center p-6 rounded-r-full rounded-l-full border-[0.5px] border-orange' >
-     <NavbarLogo/>
-        <div className={` transition-all duration-500 ${menuOpen ? "sm:block":"sm:hidden"} lg:block `}>
-
-        <NavbarLinks/>
+    <nav className="max-w-[1300px] mx-auto w-full  px-4 fixed left-[50%] -translate-x-[50%] z-20 flex gap-4 mt-2">
+      <div className="flex justify-between w-full max-w-[1200px] mx-auto  bg-black items-center p-6 rounded-r-full rounded-l-full border-orange border-[0.5px] ">
+        <NavbarLogo />
+        <div className={`${menuOpen ? "sm:block" : "sm:hidden"} lg:block`}>
+          <NavbarLinks />
         </div>
-        <NavbarBtn/>
-     </div>
 
-     <div className='flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full border-[0.5px] border-orange'>
-    <button className='text-2xl p-3 border-orange rounded-full ' onClick={toggleMenu}> 
-      <GiHamburgerMenu/></button>
-     </div>
+        <NavbarBtn />
+      </div>
+      <div className="flex lg:hidden sm:block p-6 bg-black items-center justify-center rounded-full  border-orange border-[0.5px] ">
+        <NavbarToggler />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default NavbarMain
+export default NavbarMain;
