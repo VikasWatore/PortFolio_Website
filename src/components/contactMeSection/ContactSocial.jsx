e822cf2edb2068b4ac9b0c04262944566bb154c1
VikasWatore <watorevikas@gmail.com>
2025-03-01 23:21:17 +0530

    updated Instagram Link

--- src/components/contactMeSection/ContactSocial.jsx
+++ src/components/contactMeSection/ContactSocial.jsx
@@ -8,7 +8,7 @@ const ContactSocial = () => {
     <div className='flex gap-4'>
         <SingleContactSocial Link='https://github.com/VikasWatore'  Icon={FiGithub}/>
         <SingleContactSocial Link='https://www.linkedin.com/in/vikas-watore-aa3336299' Icon={FaLinkedinIn}/>
-        <SingleContactSocial Link='#' Icon={FaInstagram}/>
+        <SingleContactSocial Link='https://www.instagram.com/_vikas_358/' Icon={FaInstagram}/>
 
     </div>
   )
